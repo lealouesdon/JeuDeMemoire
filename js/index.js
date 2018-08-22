5ae87aca6b4a5e759cca3d9db5058f3bfb45dbd1
Rhododendron1210 <lea.louesdon@gmail.com>
2018-08-22 13:23:07 +0200

    jeu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 
-function jouer() {
-  document.cookie = "jeu=1 ;domain=;path=/";
+function jouer(nb) {
+  var str1 = "jeu=";
+
+    var str3 = " ;domain=;path=/";
+    var res = str1.concat(nb, str3);
+  document.cookie = res;
 }
--- js/jeu.js
+++ js/jeu.js
@@ -1,14 +1,53 @@
 var jeu;
-
+var fichiers = [];
+var done = [];
+//fonction de départ
 $(document).ready(function(){
     getJeu();
     $("#text").text(jeu);
+    //initialisationArray();
+    addImages();
+    console.log(fichiers);
 });
+
+//recuperation du jeu lancé
 function getJeu() {
   jeu = getCookie("jeu");
 }
 
+function addImages(){
+  var row = [];
+  var n;
+  var i = 0;
+  var z = "0";
+  while (i<3) {
+    n = Math.floor((Math.random() * 100));
+    if(n<10){
+      n = z.concat(n.toString());
+    }else{
+      n = n.toString();
+    }
+    if(!done.includes(n)){
+      i++;
+      done.push(n);
+      row.push(n);
+    }
+  }
+  fichiers.push(row);
+}
 
+function initialisationArray() {
+  var i;
+  var z = "0";
+  for (i = 0; i < 100; i++) {
+    if(i<10){
+      fichiers[i] = z.concat(i.toString());
+    }else{
+      fichiers[i] = i.toString();
+    }
+  }
+}
+//pour récuperer un élement d'une cookie
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
