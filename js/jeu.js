0741d810f730e6c6982cc58d527349dc2a0fa731
Rhododendron1210 <lea.louesdon@gmail.com>
2018-08-25 16:14:33 +0200

    commit suite

--- js/jeu.js
+++ js/jeu.js
@@ -1,5 +1,8 @@
 var jeu;
 var fichiers = [];
+//copie pour l'utilisateur
+var fichiers2 = [];
+var rowTemp = [];
 //pour qu'il n'y est pas d'image identique
 var done = [];
 var clock;
@@ -8,8 +11,10 @@ var index = 0; //tete de lecture
 //fonction de départ
 $(document).ready(function(){
     getJeu();
+    $('.overlay').hide();
     $("#text").text(jeu);
-    $('.main').hide();
+    $('#sortable').hide();
+    $('#corr').hide();
     //initialisationArray();
     //addImages();
     console.log(fichiers);
@@ -25,12 +30,16 @@ $(document).ready(function(){
             }
 				});
 
+
+
 });
 
 //recuperation du jeu lancé
 function getJeu() {
   jeu = getCookie("jeu");
 }
+
+
 
 function afficheIndex(){
     var str1 = '../sources/'
@@ -48,30 +57,61 @@ function afficheIndex(){
 
 }
 
-/*document.addEventListener("keydown", function keyDown() {
-  if(etape==2){
-    addImages();
-    console.log(fichiers);
-    console.log(etape);
-    afficheLast();
-  }else{
-    console.log(fichiers);
-    console.log(etape);
+function afficheIndexMix(){
+  var str1 = '../sources/'
+  var str3 = '.jpg'
+  if (index<fichiers.length){
+    var row = fichiers[index];
+    var strf = str1.concat(row[0],str3);
+    $("#i1").attr("src",strf);
+    strf = str1.concat(row[1],str3);
+    $("#i2").attr("src",strf);
+    strf = str1.concat(row[2],str3);
+    $("#i3").attr("src",strf);
+
+    var parent = $("#sortable");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
   }
+}
 
-});*/
+//sauvegarde
+function sauvegarde() {
+  var row = [];
+  var parent = $("#sortable");
+  var imgs = parent.children();
+  var i = 0;
+  while (i<imgs.length) {
+    var str = imgs[i].getElementsByTagName("img")[0].getAttribute("src");
+    var res = str.substring(11, 13);
+    //console.log(res);
+    row.push(res);
+    i++;
+  }
+  fichiers2.push(row);
 
-//scroll event
 
-$(window).click(function(event){
-   var st = $(this).scrollTop();
+}
+//skip button
+function corr() {
+  clock.stop();
+}
+//click event
+
+$('html').click(function(event){
    if(etape==2){
       addImages();
       index +=1;
       afficheIndex();
+   }else if(etape==4){
+     //sauvegarde
+     sauvegarde();
+     index +=1;
+     afficheIndexMix();
+     //console.log(fichiers2);
    }
-
-   lastScrollTop = st;
 });
 
 function addImages(){
@@ -97,10 +137,11 @@ function addImages(){
 
   }
 }
+
 //entre étape
 function stop() {
   if(etape==1){
-    $('.main').show();
+    $('#sortable').show();
     clock = new FlipClock($('.clock'), 60, {
       clockFace: 'MinuteCounter',
       autoStart: true,
@@ -112,9 +153,9 @@ function stop() {
     etape = 2;
     addImages();
     afficheIndex();
-    console.log(etape);
+    //console.log(etape);
   }else if (etape==2) {
-    $('.main').hide();
+    $('#sortable').hide();
     clock = new FlipClock($('.clock'), 10, {
       clockFace: 'MinuteCounter',
       autoStart: true,
@@ -124,14 +165,53 @@ function stop() {
         }
     });
     etape = 3;
-    console.log(etape);
+    //console.log(etape);
   }else if (etape == 3){
-
+    $('#sortable').show();
+    $('#corr').show();
+    clock = new FlipClock($('.clock'), 180, {
+      clockFace: 'MinuteCounter',
+      autoStart: true,
+      countdown: true,
+      callbacks:{
+            stop: function() {stop();}
+        }
+    });
+    //
+    var el = document.getElementById('sortable');
+    var sortable = Sortable.create(el);
+    //console.log(sortable.toArray());
+    index = 0;
+    afficheIndexMix();
+    etape = 4;
   }else{
-
+    $('#sortable').hide();
+    $('.overlay').show();
+    verifier();
+    var str1 = "Points :";
+    var str2 = "/";
+    var str = str1.concat(points,str2,fichiers.length)
+    $("#text").text(str);
+    etape=5;
   }
-
 }
+
+//dernière étape
+var points = 0;
+function verifier() {
+  var i = 0;
+  var row1;
+  var row2;
+  while(i<fichiers2.length){
+    row1 = fichiers[i];
+    row2 = fichiers2[i];
+    if(row1[0]==row2[0]&&row1[1]==row2[1]&&row1[2]==row2[2]){
+      points+=1;
+    }
+    i++;
+  }
+}
+
 
 function initialisationArray() {
   var i;
